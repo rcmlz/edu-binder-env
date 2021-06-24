cf8c709e79a49eefc9c4d9d47518dbb9e283cf06
rcmlz <19784049+rcmlz@users.noreply.github.com>
2021-06-24 14:49:29 +0200

    Initial Commit

--- postBuild.d/cpp.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rm -rf /srv/conda/envs/notebook/share/jupyter/kernels/xcpp11
-rm -rf /srv/conda/envs/notebook/share/jupyter/kernels/xcpp17
--- postBuild.d/elm.sh
+++ postBuild.d/elm.sh
@@ -2,4 +2,5 @@
 # https://github.com/abingham/jupyter-elm-kernel
 python -m elm_kernel.install
 
+mv $HOME/ressources/elm/logo-* $HOME/.local/share/jupyter/kernels/elm/
 mv $HOME/ressources/elm/logo-* /srv/conda/envs/notebook/share/jupyter/kernels/elm/
