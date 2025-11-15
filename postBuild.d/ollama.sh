f1f2715a13aec8ce209f9fde74b3aef9666b6167
R <rcmlz@github.com>
2025-11-15 09:59:12 +0100

    Remove

--- postBuild.d/ollama.sh
+++ postBuild.d/ollama.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cd /tmp
-curl -fsSL https://ollama.com/install.sh > ollama.sh
-chmod +x ollama.sh
-./ollama.sh
-ollama --version
+#cd /tmp
+#curl -fsSL https://ollama.com/install.sh > ollama.sh
+#chmod +x ollama.sh
+#./ollama.sh
+#ollama --version
--- postBuild.d/quarto.sh
+++ postBuild.d/quarto.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-QUARTO_VERSION=1.8.26
-
-echo "Installing $QUARTO_VERSION"
-
-cd /tmp
-curl https://github.com/quarto-dev/quarto-cli/releases/download/v$QUARTO_VERSION/quarto-$QUARTO_VERSION-linux-amd64.deb
-dpkg -i quarto-$QUARTO_VERSION-linux-amd64.deb
-quarto version
+#QUARTO_VERSION=1.8.26
+#echo "Installing $QUARTO_VERSION"
+#cd /tmp
+#curl https://github.com/quarto-dev/quarto-cli/releases/download/v$QUARTO_VERSION/quarto-$QUARTO_VERSION-linux-amd64.deb
+#dpkg -i quarto-$QUARTO_VERSION-linux-amd64.deb
+#quarto version
